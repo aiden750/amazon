4d6a5b50cc8080c5f9d9857972c62c954f14c946
ashish <aidenpratt999@gmail.com>
2022-07-31 11:08:39 +0530

    learn2

---
+++ a.sh
@@ -0,0 +1,6 @@
+ there are many kinds of monster in this world.....
+ monster who devour dream, monster who suck blood, monster who abduct children........... monsters who always lies.
+ lying monsters are real nuisance,
+ They eat, even tough they never experinced hunger.
+ They study, even tough they have no interest in academic.
+ They seek friendship....... even though they have no interest in human being.
---
+++ t.sh
@@ -0,0 +1,4 @@
+ hello everyone ,
+ if you are this, you already know upto what portion I have completed
+ you see how hard it is to work on this  alll.
+  but i am trying my best and practicing hard to get control over this. it may take some time.	
