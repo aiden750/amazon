7d279e4b02859a6d80d2ce635d5d5f07578d0218
Aiden Pratt <109981056+aiden750@users.noreply.github.com>
2022-07-31 11:22:08 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -4,3 +4,5 @@
  They eat, even tough they never experinced hunger.
  They study, even tough they have no interest in academic.
  They seek friendship....... even though they have no interest in human being.
+ If I were to encounter such monster I would gonna die...
+ 
