5eb7465cdb0e9e4acf33bd9360645414440a37a7
ashish <aidenpratt999@gmail.com>
2022-07-31 14:50:47 +0530

    branch master

--- t.sh
+++ t.sh
@@ -1,5 +1,6 @@
- hello everyone ,
- if you are this, you already know upto what portion I have completed
+hello, i am again updating this line in master branch .
+hello everyone ,
+  if you are this, you already know upto what portion I have completed
  you see how hard it is to work on this  alll.
   but i am trying my best and practicing hard to get control over this. it may take some time. 
   hello everyone, this line is updating in master branch.
